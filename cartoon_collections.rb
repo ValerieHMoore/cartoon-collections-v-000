5d15ce87859a3d4ee138add124e85c253df958fa
Valerie Moore <periwinkle17@outlook.com>
2018-06-11 23:27:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(veggies)
-  veggies.split(' ').map {|w| w.capitalize + "!" }.join()
+  veggies.split(' ').map {|w| w.capitalize + "!" }.join(' ')
     end
     veggies
   end
