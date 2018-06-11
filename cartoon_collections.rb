14efe36945153d4d27b35eeb9fb4ddccb09d86b9
Valerie Moore <periwinkle17@outlook.com>
2018-06-11 23:55:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find 
-    { |i| i == "cheddar" || i == "gouda" || i == "camembert")
+  array.find { |i| i == "cheddar" || i == "gouda" || i == "camembert")
 end
