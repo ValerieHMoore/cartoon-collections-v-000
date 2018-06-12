138ea271be514514f852b4c0e3c72974025800f7
Valerie Moore <periwinkle17@outlook.com>
2018-06-12 00:06:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
   array.find { |i| i == "cheddar" || i == "gouda" || i == "camembert")
+  end
 end
